2d58b946e34a791aec1277061750230b33375b7e
Karmugilan N <160196275+Karmugilan01@users.noreply.github.com>
2025-11-23 11:43:06 +0530

    Removing footer links, Footer.jsx

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -3,12 +3,15 @@ import { assets, footer_data } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div className='px-4 md:px-16 lg:px-24 xl:px-32 bg-primary/3'>
-        <div className='flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500'>
-            <div>
-                <img src={assets.logo} alt="logo" className='w-32 sm:w-44' />
-                <p className='max-w-[410px] mt-6'>lorem ipsum dolor sit ame consectetue, adipisine elit, Rerum unde quaerat eveniet cumque accusamus atque qui error quo enim fugiat?</p>
-            </div>
+    <div className='px-4 md:px-16 lg:px-24 xl:px-32 bg-primary/3 '>
+
+        <div className='flex flex-col items-center justify-center  py-10 border-b border-gray-500/30' >
+            <img src={assets.logo} alt="logo" className='w-32 sm:w-44' />
+            <p className='max-w-[590px] mt-6 text-center'>Immerse yourself in a library of thought-provoking content where every article unlocks a new possibility. This is your exclusive destination for the rare knowledge and inspiration required to elevate your daily life.</p>
+        </div>
+
+        {/*<div className='flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500'>
+            
 
             <div className='flex flex-wrap justify-between w-full md:w-[45%] gap-5'>
                 {footer_data.map((section, index)=> (
@@ -27,7 +30,7 @@ const Footer = () => {
 
 
 
-        </div>
+        </div>*/}
         <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>Copyright 2025 QuickBlog  Karmugilan - All Right Reserved.</p>
     </div>
   )
